2faa9e3f73d3fa0ef74ee540a71758a965b8a02c
silenthunter0814 <silenthunter0814@users.noreply.github.com>
2023-04-04 12:45:10 +0000

    deploy: 793a5e8ebc35e97c57b25399ea2186ff81349325

---
+++ assets/guide_sip002.md.008d2f0a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o,a as t}from"./app.4034cd33.js";const g=JSON.parse('{"title":"SIP002 URI scheme","description":"","frontmatter":{},"headers":[],"relativePath":"guide/sip002.md","lastUpdated":1680612283000}'),a={name:"guide/sip002.md"},n=t("",20),p=[n];function r(c,i,l,d,u,h){return o(),s("div",null,p)}const f=e(a,[["render",r]]);export{g as __pageData,f as default};
