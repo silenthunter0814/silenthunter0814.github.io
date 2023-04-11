89bb2467dc55bd09c17ea619abf30287d37e4d28
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-04-12 01:45:25 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,7 @@ export default {
   lastUpdated: true,
 
   themeConfig: {
-    logo: 'https://silenthunter0814.github.io/pub/assets/sh.svg',
+    logo: 'https://silenthunter0814.github.io/pub/assets/sh.png',
 
     nav: nav(),
 
