85d1f199ee723602f03a8f1b71f34d04d7635844
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-04-12 02:11:57 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,6 +24,12 @@ export default {
     },
 
     socialLinks: [
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+        },
+        link: 'https://space.bilibili.com/1551957972'
+      },
       { icon: 'youtube', link: 'https://www.youtube.com/@silenthunter0814' },
       { icon: 'github', link: 'https://github.com/silenthunter0814/silenthunter0814.github.io' }
     ],
